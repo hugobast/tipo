da8dedab3413eaf98e08f0077d10b850e1b38f8b
Hugo Bastien <hugobast@gmail.com>
2013-03-12 09:05:43 -0400

    We can now derive a simple feature list from the font file

--- fonte.rb
+++ fonte.rb
@@ -21,7 +21,7 @@ class Fonte
   include Constants
 
   def initialize file
-    @file = File.open file
+    @file = File.open file, "rb:ASCII-8BIT"
   end
 
   def offset_table
@@ -31,12 +31,35 @@ class Fonte
   end
 
   def tables 
-    accumulator = []
+    entries = []
     @file.seek table_records_begin 
     offset_table.tables_count.times do
       chunk = @file.read table_record_size
-      accumulator << TableRecord.new(*(chunk.unpack 'a4N3'))
+      entries << TableRecord.new(*(chunk.unpack 'a4N3'))
     end
-    accumulator
-  end 
+    entries 
+  end
+
+  def feature_list
+    seek_to_table 'GSUB'
+    gsub_header = @file.read(10).unpack 'n5'
+    feature_list_offset = gsub_header[3] 
+
+    @file.seek(feature_list_offset + find_table('GSUB').offset)
+    
+    feature_count = @file.read(2).unpack 'n'
+    acc = []
+    feature_count[0].times do
+      acc << @file.read(6).unpack('a4n')[0]
+    end
+    acc.uniq
+  end
+
+  def seek_to_table tag
+    @file.seek find_table(tag).offset
+  end
+
+  def find_table tag
+    tables.select { |t| t.tag == tag }.first
+  end
 end
--- spec/fonte_spec.rb
+++ spec/fonte_spec.rb
@@ -7,9 +7,22 @@ describe Fonte do
       fonte.offset_table.version.should eq "OTTO"
     end
 
-    it "reads the table tables" do
+    it "reads the table record entries" do
       fonte = Fonte.new 'spec/hobo.otf'
       fonte.tables.first.tag.should eq "BASE"
     end
+  end
+
+  context "reads simple font information" do
+    it "reads the feature list" do
+      fonte = Fonte.new 'spec/hobo.otf'
+      fonte.feature_list.should eq ['aalt', 'frac', 'liga', 'ordn', 'sups']
+    end
+
+    it "reads the font name"
+
+    it "reads the font style"
+
+    it "reads the font family name"
   end
 end
